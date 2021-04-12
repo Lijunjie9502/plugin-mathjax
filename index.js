59402dc6bc4209c12a1d8c15064c77373286b62e
Lijunjie9502 <48000576+Lijunjie9502@users.noreply.github.com>
2021-04-12 17:33:01 +0800

    Update index.js
    handle the status of "$"

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ module.exports = {
     website: getWebsiteAssets,
     hooks:{
         "page:before": function(page) {
-        const regex = /((?<!\$)[\$](?!\$).*?(?<!\$)[\$](?!\$))/gm;
+        const regex = /((?<![\$"])[\$](?![\$"]).*?(?<![\$"])[\$"](?![\$"]))/gm;
         page.content = page.content.replace(regex, "$$$1$$");
         const regex_pdf_link = /\[(.*)\]\((.*\.pdf)\)/gm;
         const subst_pdf_link = `<a href="$2" target='_blank'> $1 </a>`;
